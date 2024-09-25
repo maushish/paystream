c1cdf24f16868e5a0fb909e93320cca20c75ebfd
maushish <chamarnumber102@gmail.com>
2024-09-26 01:28:32 +0530

    url bug

---
+++ web/app/actions.json/route.ts
@@ -0,0 +1,24 @@
+import { ACTIONS_CORS_HEADERS, ActionsJson } from "@solana/actions";
+
+export const GET = async () => {
+  const payload: ActionsJson = {
+    rules: [
+      
+      {
+        pathPattern: "/*",
+        apiPath: "/api/paystream_api/*",
+      },
+     
+      {
+        pathPattern: "/api/paystream_api/**",
+        apiPath: "/api/paystream_api/**",
+      },
+    ],
+  };
+
+  return Response.json(payload, {
+    headers: ACTIONS_CORS_HEADERS,
+  });
+};
+ 
+export const OPTIONS = GET;
